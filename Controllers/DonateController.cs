4318fe0207e680350f88ec10266480124e819f37
Charlie <Cha9872145@gmail.com>
2025-07-25 12:51:53 +0800

    加入專案檔案。

---
+++ Controllers/DonateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test1.Controllers
+{
+    public class DonateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
